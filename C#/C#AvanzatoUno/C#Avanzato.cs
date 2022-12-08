12e94650fbe8276494a3ecb7e67036bb7d8c64ab
GioVialee <panciainsotto@gmail.com>
2022-12-08 23:52:41 +0100

    Aggiornata la cartella C#

--- C#/C#AvanzatoUno/C#Avanzato.cs
+++ C#/C#AvanzatoUno/C#Avanzato.cs
@@ -7,27 +7,31 @@ class Avanzato{
 // Read-Only Proprieties:
     int readOnly = 10;
     public int ReadOnly{
-        get { return (readOnly * 2); } //la variabile ha solo metodo get e riporta il valore di "readOnly"
+        get { return (readOnly * 2); } //la variabile ha solo metodo get e riporta il valore di "readOnly * 2"
     } 
 
 //Write-Only Proprieties:
     int wrtiteOnly = 20;
     public int WriteOnly{
-        set { wrtiteOnly = wrtiteOnly*value;} //la variabile ha solo metodo get e modifica il valore di "writeOnly"
+        set { wrtiteOnly = wrtiteOnly*value;} //la variabile ha solo metodo get e modifica il valore di "writeOnly  * value"
     }
 
 // Expression-Bodied Proprieties: Le proprietà possono essere scritte anche in modo differente, una freccia può rimpiazzare tutte
 // Le parentesi che utilizziamo normalmente nella dichiarazione di una proprietà e i "return" keywords
     int prova = 90;
     public int expressionBodied{
-        get => prova * 20;
+        get => prova * 20; //La proprietà ha il metodo get e riporta il valore di "prova * 2"
         set => prova = prova * value;
     }
 
 // Automatic proprieties: La proprietà automatica permette all'utente di accedere alla variabile in maniera diretta senza dover
 // Specificare get, set e le relative variabili e soprattutto senza il keycode "value"
 public int automaticProprieties{get; set;}  //La proprietà automatica non usa variabili esterne
-//                                            Di fatti usa il valore di se stessa.
+//                                            Di fatti usa il valore di se stessa. Il set cambierà il suo valore.
+//per crearne una con un valore inizializzato diverso da 0 si usa la seguente sintassi piscio:
+public int proprietàDiProva {get; set;} = 100; //Inizializzo la variabile a 100
+//In entrambi i casi il keycode "private" renderebbe inutilizzabile la funzione get o set che sia per rendere
+//La proprietà Read-Only oppure Wrtite-Only
 
 
 
@@ -41,5 +45,6 @@ public static void Main(string[] args)
     alekish.automaticProprieties = 900;
     alekish.automaticProprieties += 800;
     Console.WriteLine("La variabile di automaticProprieties è: " + alekish.automaticProprieties);
+    Console.WriteLine("La variabile della proprietàDiProva è: " + alekish.proprietàDiProva);
 }
 }
