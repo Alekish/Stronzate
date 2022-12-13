046c5adcf91e3c316055102b630a3e9f9c257c88
GioVialee <panciainsotto@gmail.com>
2022-12-13 23:46:16 +0100

    Aggiornata la cartella C#, quattro va finito

---
+++ MAIN PROGRAMS/C#/C#AvanazatoQuattro/Program.cs
@@ -0,0 +1,18 @@
+﻿//DOWNCAST E UPCAST
+
+//L'upcasting è un operazione che avviene senza dover specificare l'operazione e si basa sul
+//promuovere una variabile o un tipo ad uno più vasto.
+//esempio:
+int a = 90;
+long b = a;     //Upcasting, metodo lecito
+
+//Il downcasting è un operazione che avviene in modo esplicito e si basa sull'opposto dell'upcasting
+//retrocede una variabile o un tipo ad un tipo molto meno vasto.
+a = (int) b;    //Downcasting, metodo lecito ed esplicito
+
+//AS OPERATOR
+//l'operatore as effettua downcast in null se il downcast da effettuare è impossibilitato
+string c = null;
+string d = c as string;
+Console.WriteLine(d); //Non stamperà nulla
+
